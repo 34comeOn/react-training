bd0a8c6ddf4ef425d117ff9596852fc0ee759d46
34comeOn <80600393+34comeOn@users.noreply.github.com>
2022-07-21 11:35:25 +0300

    Developes header, navigation

--- my-app/src/components/layout/layout.tsx
+++ my-app/src/components/layout/layout.tsx
@@ -1,9 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
       <header>
+        <Link to="/" title="/">
+            <h3>Logo place</h3>
+        </Link>
+        <nav>
+            <Link to="/history" title="/history">
+                History
+            </Link>
+            <Link to="/selected" title="/selected">
+                Selected
+            </Link>
+            <Link to="/" title="/">
+                Main
+            </Link>
+            <Link to="/signup" title="/signup">
+                Register
+            </Link>
+            <Link to="/login" title="/login">
+                Sign in
+            </Link>
+            <a href="#">
+                Exit
+            </a>
+        </nav>
       </header>
       <main>
         <Outlet />
