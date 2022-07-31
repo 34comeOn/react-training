5571eff23fe896eb4b7b1707362c0c44cb45c67a
34comeOn <80600393+34comeOn@users.noreply.github.com>
2022-07-31 07:15:31 +0300

    testing

--- my-app/src/utils/dataAPI.ts
+++ my-app/src/utils/dataAPI.ts
@@ -8,9 +8,7 @@ function fetchData(currentYear = '2019') {
       headers: {
         'X-RapidAPI-Key': '2f2d49651cmsh722c2e352245ab7p18b115jsn240c8d3c8935',
         'X-RapidAPI-Host': 'formula-18.p.rapidapi.com'
-      }});
-
-  
+      }}); 
 }
 
 export const getDataAsync = createAsyncThunk(
