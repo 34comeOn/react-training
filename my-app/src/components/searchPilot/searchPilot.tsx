45b465e53a590ab60f33b7339fa580ed182926e7
34comeOn <80600393+34comeOn@users.noreply.github.com>
2022-08-04 06:40:44 +0300

    adds pilot subSearch

--- my-app/src/components/searchPilot/searchPilot.tsx
+++ my-app/src/components/searchPilot/searchPilot.tsx
@@ -1,4 +1,5 @@
 import { dataAPI } from "../../utils/dataAPI";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import CardPilot from "../cardPilot/cardPilot";
@@ -7,18 +8,31 @@ import s from './searchPilot.module.css';
 
 
 export default function SearchPilot (): JSX.Element {
+    const [inputState, setInputState] = useState('');
     let [searchParam]: any = useSearchParams();
 
     const {data: posts} = dataAPI.useFetchPilotQuery(
     searchParam.get("year"));
 
     return ( 
-        <div className={s.pilotsWrapper}> 
+        <>
+            <label htmlFor="pilotSubSearch">
+                <b style={{fontSize: "20px", margin: "20px 10px", display: "inline-block"
+                }}>
+                Find your pilot in list:</b>
+            </label>
+            <input type="text" id="pilotSubSearch" value={inputState} 
+                onChange= {(event) => setInputState(event.target.value)} style={{fontSize: "20px"}}/>
+            
+            <div className={s.pilotsWrapper}> 
 
-            {posts?.map((card:any) => 
-                <CardPilot key={card.POS} pos={card.POS} pts={card.PTS} name={card.Name} 
-                country={card.Country} car={card.Car}/>
-            )}
-        </div>
+                {posts?.filter((card: any) => card.Name.toLowerCase().includes(inputState.toLowerCase())).map((card:any) => 
+                    <CardPilot key={card.POS} pos={card.POS} pts={card.PTS} name={card.Name} 
+                    country={card.Country} car={card.Car}/>
+                )
+                
+                }
+            </div>
+        </>
     )
 }
